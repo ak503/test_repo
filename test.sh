a662a4b5329a602b118986e665aef3e0832a91b8
dturlupov <dturlupov@factor-ts.ru>
2017-01-23 17:49:42 +0300

    step 2-4 TODO

--- test.sh
+++ test.sh
@@ -1,28 +1,16 @@
 #!/bin/sh
 
-home_dir="$HOME/test_turlupov/"
+home_dir="$HOME/test_turlupov"
+dir_for_cp="$HOME/test_turlupov/for_cp"
+dir_for_mv="$HOME/test_turlupov/for_mv"
+
 maxdir=5
 maxfiles=100
 
-Step1()
-{
-	dir="$1"
-	maxnum="$2"
-	for i in `seq 1 $maxnum`
-	do
-		mkdir "$home_dir""$i" 2>>/dev/null
-		if [ "x$?" = "x1" ] ; then
-			return 1
-		fi
-	done
-	return 0
-}
-
 if [ -d $home_dir ] ; then
-	rm -rf "$home_dir"*
-else
-	mkdir "$home_dir"
+	rm -rf "$home_dir"
 fi
+mkdir "$home_dir"
 
 #TODO check
 
@@ -42,7 +30,7 @@ echo "Step 1:"
 for i in `seq 1 $maxdir`
 do
 	#echo "$i"
-	mkdir "$home_dir""$i" 2>>/dev/null
+	mkdir "$home_dir"/"$i"
 	if [ "x$?" = "x1" ] ; then
 		echo "\tFAILED"
 		exit 1
@@ -51,18 +39,38 @@ done
 echo "\tPASSED"
 ########################
 
+######## step 2 ########
+echo "Step 2:"
+for i in `ls $home_dir` ; do
+	for j in `seq 1 $maxfiles` ; do
+		touch $home_dir/$i/$j
+		if [ "x$?" = "x1" ] ; then
+			echo "\tFAILED touch"
+			exit 1
+		fi
+		dd if=/dev/urandom of=$home_dir/$i/$j bs=100 count=1 1>/dev/null 2>/dev/null
+		if [ "x$?" = "x1" ] ; then
+			echo "\tFAILED dd"
+			exit 1
+		fi
+	done
+done
+echo "\tPASSED"
+
+########################
+return 0
+
+
+
+
 ####### step 2-4 #######
 echo "Step 2-4:"
-for i in `ls $home_dir`
-do
-	#echo "in dir $home_dir$i"
+for i in `ls $home_dir` ; do
 
-	for j in `seq 1 $maxfiles`
-	do
+	for j in `seq 1 $maxfiles` ; do
 		filename="$home_dir$i/$j""_$i""k_"
 		d=`date +%T_%d-%m-%Y` #hh:mm:ss_d-m-y
 		filename="$filename""$d"
-		#touch "$filename"
 		dd if=/dev/urandom of=$filename bs=1k count=$i	1>/dev/null 2>/dev/null
 	done
 done
@@ -71,8 +79,6 @@ echo "\tPASSED"
 
 ######## step 5 ########
 echo "Step 5:"
-dir_for_cp="$home_dir""for_cp"
-
 if [ -d $dir_for_cp ] ; then
 	rm -rf "$dir_for_cp"
 fi
@@ -96,7 +102,6 @@ echo "\tPASSED"
 
 ######## step 6 ########
 echo "Step 6:"
-dir_for_mv="$home_dir""for_mv"
 if [ -d $dir_for_mv ] ; then
 	rm -rf "$dir_for_mv"
 fi
