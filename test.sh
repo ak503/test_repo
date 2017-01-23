5fd1f64ba825c8367fbe3380d4839003acccfdff
dturlupov <dturlupov@factor-ts.ru>
2017-01-23 15:17:37 +0300

    cosmetics

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 home_dir="$HOME/test_turlupov/"
+maxdir=5
+maxfiles=100
 
 Step1()
 {
@@ -25,31 +27,32 @@ fi
 #TODO check
 
 ## step 1 ##
-echo "step 1.."
-Step1 "$home_dir" 5
-if [ "x$?" = "x1" ] ; then
-	echo "step 1 FAILED"
-	exit 1
-fi
-echo "step 1 PASSED"
+#echo "step 1.."
+#Step1 "$home_dir" 5
+#if [ "x$?" = "x1" ] ; then
+#	echo "step 1 FAILED"
+#	exit 1
+#fi
+#echo "step 1 PASSED"
 
-return 0
+#return 0
 
-
-echo "step 1.."
-maxdir=5
+######## step 1 ########
+echo "Step 1:"
 for i in `seq 1 $maxdir`
 do
 	#echo "$i"
 	mkdir "$home_dir""$i" 2>>/dev/null
 	if [ "x$?" = "x1" ] ; then
-		echo "step 1 FAILED"
+		echo "\tFAILED"
 		exit 1
 	fi
 done
-echo "step 1 PASSED"
-echo "step 2-4.."
-maxfiles=100
+echo "\tPASSED"
+########################
+
+####### step 2-4 #######
+echo "Step 2-4:"
 for i in `ls $home_dir`
 do
 	#echo "in dir $home_dir$i"
@@ -58,66 +61,73 @@ do
 	do
 		filename="$home_dir$i/$j""_$i""k_"
 		d=`date +%T_%d-%m-%Y` #hh:mm:ss_d-m-y
-		#d= "`echo `"
 		filename="$filename""$d"
-		#echo "$filename"
-		touch "$filename"
+		#touch "$filename"
 		dd if=/dev/urandom of=$filename bs=1k count=$i	1>/dev/null 2>/dev/null
 	done
 done
-echo "step 2-4 PASSED"
-echo "step 5.."
-## step 5 ##
+echo "\tPASSED"
+########################
+
+######## step 5 ########
+echo "Step 5:"
 dir_for_cp="$home_dir""for_cp"
-if [ -d $dir_for_cp ]
-then
+
+if [ -d $dir_for_cp ] ; then
 	rm -rf "$dir_for_cp"
 fi
+
 mkdir "$dir_for_cp"
 if [ "x$?" = "x1" ] ; then
-	echo "step 5 FAILED: mkdir fail"
+	echo "\tFAILED: mkdir fail"
 	exit 1
 fi
-for i in `seq 1 $maxdir`
-do
+
+for i in `seq 1 $maxdir` ; do
 	cp -r "$home_dir$i" "$dir_for_cp"
 	if [ "x$?" = "x1" ] ; then
-		echo "step 5 FAILED: cp fail"
-	exit 1
-fi
+		echo "\tFAILED: cp fail"
+		exit 1
+	fi
 done
-echo "step 5 PASSED"
-echo "step 6.."
-## step 6 ##
+
+echo "\tPASSED"
+########################
+
+######## step 6 ########
+echo "Step 6:"
 dir_for_mv="$home_dir""for_mv"
-if [ -d $dir_for_mv ]
-then
+if [ -d $dir_for_mv ] ; then
 	rm -rf "$dir_for_mv"
 fi
+
 mkdir "$dir_for_mv"
 if [ "x$?" = "x1" ] ; then
-	echo "step 6 FAILED: mkdir fail"
+	echo "\tFAILED: mkdir fail"
 	exit 1
 fi
 
 mv "$dir_for_cp" "$dir_for_mv"
 if [ "x$?" = "x1" ] ; then
-	echo "step 6 FAILED: mv fail"
+	echo "\tFAILED: mv fail"
 	exit 1
 fi
-echo "step 6 PASSED"
+echo "\tPASSED"
+########################
 
-## step 7 ##
-echo "step 7.."
+######## step 7 ########
+echo "Step 7:"
 for i in `seq 1 $maxdir`
 do
 	rm -rf "$home_dir""$i" 2>>/dev/null
 	if [ "x$?" = "x1" ] ; then
-		echo "step 7 FAILED"
+		echo "\tFAILED"
 		exit 1
 	fi
 done
-echo "step 7 PASSED"
+echo "\tPASSED"
+########################
+return 0
 #echo "step 2-4.."
 #expr 6 % 2
 
