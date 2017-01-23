8f2483122c4f0cc99fea7f46b5e3d10ef2801152
dturlupov <turlupov@gmail.com>
2017-01-23 22:56:15 +0600

    refactoring

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ dir_for_cp="$HOME/test_turlupov/for_cp"
 dir_for_mv="$HOME/test_turlupov/for_mv"
 
 maxdir=5
-maxfiles=10
+maxfiles=1000
 
 if [ -d $home_dir ] ; then
 	rm -rf "$home_dir"
@@ -15,10 +15,8 @@ mkdir "$home_dir"
 
 ######## step 1 ########
 echo "Step 1:"
-for i in `seq 1 $maxdir`
-do
-	#echo "$i"
-	mkdir "$home_dir"/"$i"
+for i in `seq 1 $maxdir` ; do
+	mkdir "$home_dir"/"$i" 2>/dev/null
 	if [ "x$?" = "x1" ] ; then
 		echo "\tFAILED"
 		exit 1
@@ -55,12 +53,16 @@ echo "\tPASSED"
 ####### step 2-4 #######
 echo "Step 2-4:"
 for i in `ls $home_dir` ; do
-
 	for j in `seq 1 $maxfiles` ; do
-		filename="$home_dir/$i/$j""_$i""k_"
+		filename=$j"_"$i"k_"
 		d=`date +%T_%d-%m-%Y` #hh:mm:ss_d-m-y
-		filename="$filename""$d"
-		dd if=/dev/urandom of=$filename bs=1k count=$i	1>/dev/null 2>/dev/null
+		filename="$filename""$d" #num_size_hh:mm:ss_d-m-y
+		dd if=/dev/urandom of="$home_dir/$i/$filename" bs=1k count=$i 1>/dev/null 2>/dev/null
+
+		if [ "x$?" = "x1" ] ; then
+			echo "\tFAILED"
+		exit 1
+	fi
 	done
 done
 echo "\tPASSED"
@@ -72,14 +74,14 @@ if [ -d $dir_for_cp ] ; then
 	rm -rf "$dir_for_cp"
 fi
 
-mkdir "$dir_for_cp"
+mkdir "$dir_for_cp" 2>/dev/null
 if [ "x$?" = "x1" ] ; then
 	echo "\tFAILED: mkdir fail"
 	exit 1
 fi
 
 for i in `seq 1 $maxdir` ; do
-	cp -r "$home_dir/$i" "$dir_for_cp"
+	cp -r "$home_dir/$i" "$dir_for_cp" 2>/dev/null
 	if [ "x$?" = "x1" ] ; then
 		echo "\tFAILED: cp fail"
 		exit 1
@@ -95,13 +97,13 @@ if [ -d $dir_for_mv ] ; then
 	rm -rf "$dir_for_mv"
 fi
 
-mkdir "$dir_for_mv"
+mkdir "$dir_for_mv" 2>/dev/null
 if [ "x$?" = "x1" ] ; then
 	echo "\tFAILED: mkdir fail"
 	exit 1
 fi
 
-mv "$dir_for_cp" "$dir_for_mv"
+mv "$dir_for_cp" "$dir_for_mv" 2>/dev/null
 if [ "x$?" = "x1" ] ; then
 	echo "\tFAILED: mv fail"
 	exit 1
@@ -111,9 +113,8 @@ echo "\tPASSED"
 
 ######## step 7 ########
 echo "Step 7:"
-for i in `seq 1 $maxdir`
-do
-	rm -rf "$home_dir""$i" 2>>/dev/null
+for i in `seq 1 $maxdir` ; do
+	rm -r "$home_dir"/"$i" 2>/dev/null
 	if [ "x$?" = "x1" ] ; then
 		echo "\tFAILED"
 		exit 1
@@ -128,21 +129,26 @@ echo "\tPASSED"
 #TODO
 ######## step 7 ########
 echo "Step 9:"
-for i in `ls $dir_for_mv/for_cp`
-do
-	#echo "$i"
+
+if [ ! -d $dir_for_mv/for_cp ] ; then
+	echo "\tFAILED: test -d"
+	exit 1
+fi
+
+for i in `ls $dir_for_mv/for_cp` ; do
 	usedir=`expr $i % 2`
 	if [ "x$usedir" = "x0" ] ; then
-		#echo "$i use"
 		for j in `ls $dir_for_mv/for_cp/$i` ; do
-			#echo "name= $j"
-			b=`echo "$j" |sed  s/^[0-9]*/""/`
-			#echo "b= $b"
-			name=`echo "$j" |sed  s/"$b"/""/`
-			#echo "name2= $name"
+			tmp=`echo "$j" |sed  s/^[0-9]*/""/`
+			name=`echo "$j" |sed  s/"$tmp"/""/`
+
 			delfile=`expr $name % 2`
 			if [ "x$delfile" != "x0" ] ; then
-				rm -rf  $dir_for_mv/for_cp/$i/$j
+				rm -r $dir_for_mv/for_cp/$i/$j 2>/dev/null
+				if [ "x$?" = "x1" ] ; then
+					echo "\tFAILED rm"
+					exit 1
+				fi
 			fi
 		done
 	fi
