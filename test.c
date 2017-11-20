f14382bd34efb09e12e4c35ae6f71a8a8e437916
dturlupov <dturlupov@factor-ts.ru>
2017-11-20 11:35:56 +0300

    bubbl

--- test.c
+++ test.c
@@ -1,26 +1,45 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <syslog.h>
+
+#define MAX_SIZE 50
+
+static void printMAS(char* mas, int size) {
+	int i;
+	for (i = 0; i < size; i++)
+		printf("%c",  mas[i]);
+	printf("\n");
+	return;
+}
+
+static void  sortBubb (char *mas, int size)
+{
+	char curretn,t;
+	int i,j;
+	for ( i = 0; i < size ; i++) {
+		for (j = 1; j < size-i ; j++) {
+			if (mas[j] < mas[j-1]){
+				t=mas[j];
+				mas[j] = mas[j-1];
+				mas[j-1] = t;
+			}
+		}
+	}
+	//printMAS(mas, 3);
+	return;
+}
+
 
 int main(int argc, char **argv)
 {
-	//int rc = 1;
-	printf("Main\n");
-
-	fflush(stdin);
-	fflush(stdout);
-	FILE * f;
-	//f = fdopen(0, "r");
-	char *buf;
-	while(1){
-		f = fdopen(1, "r");
-		if( f )
-	{		fread(buf, 256, 1, f);
-			printf("%s", buf);
-			fclose(f);
-		
-	}	//return 0;
-
-	}
+	char mas[MAX_SIZE] = "wefwqejihgfedcbawefqwfeadcadsaeaecadcqecaeacdscsadcwqe";
+	int i = 0;
+	
+	printMAS(mas, MAX_SIZE);
+	sortBubb(mas, MAX_SIZE);
+	printf("------------\n");
+	printMAS(mas, MAX_SIZE);
+	
 	return 0;
 }
